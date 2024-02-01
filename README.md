6ffb18ab6c5b80c44d9d96d3061565c6837f642e
CryptoSnowPrince <topdirector2017@gmail.com>
2024-02-01 20:08:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Sherpswap Smart Contracts
+
+## 1. Environment
+
+```text
+node v18.18.2
+npm 9.8.1
+```
+
+## 2. Config .env file
+
+Please copy .env.example to .env and copy your MNEMONIC and MNEMONIC_PASSWORD.
+
+```text
+MNEMONIC='your mnemonic'
+MNEMONIC_PASSWORD='your password'
+```
+
+## 3. Install packages
+
+```shell
+npm install
+```
+
+## 4. Compile Smart Contracts
+
+```shell
+npm run compile
+```
+
+## 5. Deploy Smart Contracts
+
+```shell
+npm run deploy
+```
---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+// Config from environment
+const mnemonicPhrase = process.env.MNEMONIC || 'test test test test test test test test test test test junk';
+const mnemonicPassword = process.env.MNEMONIC_PASSWORD;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.5.16',
+      },
+      {
+        version: '0.6.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+      {
+        version: '0.8.20',
+      },
+    ]
+  },
+  defaultNetwork: "evt",
+  networks: {
+    evt: {
+      url: 'http://158.69.35.30:8545',
+      accounts: {
+        mnemonic: mnemonicPhrase,
+        path: 'm/44\'/60\'/0\'/0',
+        initialIndex: 0,
+        count: 1,
+        passphrase: mnemonicPassword,
+      },
+      gasPrice: 1500000000,
+    }
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  mocha: {
+    timeout: 0,
+  },
+  etherscan: {
+    apiKey: {
+      evt: "0000000000000000000000000000000000",
+    },
+    customChains: [
+      {
+        network: "evt",
+        chainId: 8848,
+        urls: {
+          apiURL: "https://evtscan.com/api/",
+          browserURL: "https://evtscan.com/"
+        }
+      },
+    ]
+  },
+};
