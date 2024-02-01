5c961720692a8a8b0c086ba35e821b5d37945ea7
CryptoSnowPrince <topdirector2017@gmail.com>
2024-02-01 22:05:46 +0100

    add multicall3

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,6 +22,15 @@ module.exports = {
         },
       },
       {
+        version: '0.8.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.8.20',
       },
     ]
